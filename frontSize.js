b22be80a6c9ace4d493e5367703b7056a8c25a31
Diyin Hu <dhu23@uic.edu>
2021-09-24 15:15:22 -0500

    add button change front size

---
+++ frontSize.js
@@ -0,0 +1,17 @@
+
+
+function smallFontSize() {
+  var containerEle = document.getElementById('myContainer');
+  console.log(containerEle)
+  containerEle.className = "size-small";
+}
+function largeFontSize() {
+  var containerEle = document.getElementById('myContainer');
+
+  containerEle.className = "size-large";
+}
+function normalFontSize() {
+  var containerEle = document.getElementById('myContainer');
+
+  containerEle.className = "size-normal";
+}
