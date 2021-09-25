1e689cff639a9e05dbc1d4d9635d0db97ef9aa6a
Fahad <fahmad27@uic.edu>
2021-09-24 21:01:09 -0500

    removed unused file

--- helperFont.js
+++
@@ -1,7 +0,0 @@
-function setFontSize(value){
-//    get mycontainer element
-    var containerName = document.getElementById("myContainer");
-    containerName.className = "size-"+value.toLowerCase();
-    console.log(containerName.className);
-
-}
